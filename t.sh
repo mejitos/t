27f15d599c87d506c177e591c55ba7afe0b8b961
Timo Mehto <timomehto@gmail.com>
2021-05-02 03:11:55 +0300

    Added command buildtest for building the test compiler

--- t.sh
+++ t.sh
@@ -2,9 +2,9 @@
 
 SRC_DIR=./src
 BUILD_DIR=./build
-EXECUTABLE=t.exe
+EXECUTABLE=t
 TEST_BUILD_DIR=./tests/build
-TEST_EXECUTABLE=t_test.exe
+TEST_EXECUTABLE=t_test
 COMMAND=$1
 USAGE="
 Usage: t.sh <command> [options] [flags]
@@ -12,14 +12,15 @@ Usage: t.sh <command> [options] [flags]
 Commands:
     help:       Show the help/usage message
     build:      Builds the compiler in the build directory
-    run:        Builds the compiler and runs the created executable
+    buildtest:  Builds the compiler with the test modules in the test build directory
+    run:        Builds the compiler and runs the compiler with passed options and flags
     test:       Builds the compiler in the tests directory and runs the tests
-    vgrun:      Builds the compiler and runs the program with Valgrind analysis
-    vgtest:     Builds the compiler in the testst directory and runs the tests with Valgrind analysis
+    vgrun:      Command run but with Valgrind analysis. Valgrind report goes to file vglog.
+    vgtest:     Command test but with Valgrind analysis. Valgrind report goes to file vglog.
 "
 
 if [ $# -le 0 ]; then
-    echo -e "Invalid number of arguments\n$USAGE"
+    echo -e "Error: Missing or invalid command\n$USAGE"
     exit 1
 fi
 
@@ -33,15 +34,44 @@ build () {
     [ -d "$1" ] || mkdir "$1"
 
     # Compile the source code to executable if there is entry point created
-    [ -f "$2" ] && gcc -O3 -g -o $3 $4
+    [ -f "$2" ] && gcc -O3 -o $3 $4
 }
 
 
 # Run different commands based on passed argument
-# TODO(timo): Add command for valgrind analysis
 case $COMMAND in
     help )
         echo -e "$USAGE"
+        exit 0;;
+    build )
+        build "$BUILD_DIR" "$SRC_DIR/main.c" "$BUILD_DIR/$EXECUTABLE" "$SRC_DIR/*.c"
+        exit 0;;
+    buildtest )
+        build "$TEST_BUILD_DIR" "tests/main.c" "$TEST_BUILD_DIR/$TEST_EXECUTABLE" "tests/*.c 
+                                                                                   src/t.c 
+                                                                                   src/lexer.c 
+                                                                                   src/scope.c 
+                                                                                   src/array.c 
+                                                                                   src/parser.c 
+                                                                                   src/resolver.c 
+                                                                                   src/interpreter.c 
+                                                                                   src/instruction.c 
+                                                                                   src/ir_generator.c 
+                                                                                   src/ir_runner.c 
+                                                                                   src/code_generator.c 
+                                                                                   src/stringbuilder.c 
+                                                                                   src/ast.c 
+                                                                                   src/symbol.c 
+                                                                                   src/type.c 
+                                                                                   src/value.c 
+                                                                                   src/token.c 
+                                                                                   src/memory.c 
+                                                                                   src/common.c 
+                                                                                   src/diagnostics.c"
+        exit 0;;
+    run )
+        build "$BUILD_DIR" "$SRC_DIR/main.c" "$BUILD_DIR/$EXECUTABLE" "$SRC_DIR/*.c"
+        $BUILD_DIR/$EXECUTABLE "${@:2}"
         exit 0;;
     test )
         build "$TEST_BUILD_DIR" "tests/main.c" "$TEST_BUILD_DIR/$TEST_EXECUTABLE" "tests/*.c 
@@ -66,13 +96,6 @@ case $COMMAND in
                                                                                    src/common.c 
                                                                                    src/diagnostics.c"
         $TEST_BUILD_DIR/$TEST_EXECUTABLE "${@:2}"
-        exit 0;;
-    run )
-        build "$BUILD_DIR" "$SRC_DIR/main.c" "$BUILD_DIR/$EXECUTABLE" "$SRC_DIR/*.c"
-        $BUILD_DIR/$EXECUTABLE "${@:2}"
-        exit 0;;
-    build )
-        build "$BUILD_DIR" "$SRC_DIR/main.c" "$BUILD_DIR/$EXECUTABLE" "$SRC_DIR/*.c"
         exit 0;;
     vgrun )
         build "$BUILD_DIR" "$SRC_DIR/main.c" "$BUILD_DIR/$EXECUTABLE" "$SRC_DIR/*.c"
@@ -104,4 +127,4 @@ case $COMMAND in
         exit 0;;
 esac
 
-echo -e "Invalid command\n$USAGE"
+echo -e "Error: Invalid command\n$USAGE"
