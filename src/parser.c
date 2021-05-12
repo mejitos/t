42c395f7d1942a5b3e1993ba3f6568cff1490a3d
Timo Mehto <timomehto@gmail.com>
2021-05-12 16:04:17 +0300

    Separated the parsing of the parameter list. Updated docstrings.

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,19 @@
+// Implementation of parser which creates an abstract syntax tree from the 
+// token stream created by the lexer.
 //
-// TODO(timo): Filedocstring
+// At the moment there is a big problem with functions and their implementation
+// as expressions. The parsing without context became a huge obstacle because
+// the parenthesized expression can be pretty much identical to the parameter
+// list of the function. The separation between the two and especially the
+// error handling between the two became big headache. Wasted 4 days for just
+// that thing alone without any decent solutions.
 //
+// Also the error recovery is pretty bad because it is only done at statement
+// and declaration level. This created big problems especially when parsing
+// the current function expressions.
+//
+// Author: Timo Mehto
+// Date: 2021/05/12
 
 #include "t.h"
 
@@ -87,13 +100,13 @@ static void panic_mode(Parser* parser)
 }
 
 
-// TODO
+// Checks if the current token is intended and creates a diagnostic if it is not.
 //
 // Arguments
-//      parser:
-//      kind:
-//      lexeme:
-//      panic:
+//      parser: Initialized parser.
+//      kind: Expected token kind.
+//      lexeme: Lexeme of the expected token kind.
+//      panic: If the error recovery should be done or not.
 static void expect_token(Parser* parser, const Token_Kind kind, const char* lexeme, const bool panic)
 {
     // NOTE(timo): The boolean 'panic' tells whether it is time to panic or not.
@@ -106,7 +119,7 @@ static void expect_token(Parser* parser, const Token_Kind kind, const char* lexe
     {
         Diagnostic* _diagnostic = diagnostic(
             DIAGNOSTIC_ERROR, parser->current_token->position, 
-            ":PARSER - SyntaxError: Invalid token '%s', expected '%s'\n", 
+            ":PARSER - SyntaxError: Invalid token '%s', expected '%s'", 
             parser->current_token->lexeme, lexeme);
         array_push(parser->diagnostics, _diagnostic); 
         parser->panic = panic;
@@ -116,7 +129,7 @@ static void expect_token(Parser* parser, const Token_Kind kind, const char* lexe
 }
 
 
-// TODO
+// Checks the next token without advancing the pointer.
 //
 // Arguments
 //      parser: Pointer to a initialized Parser.
@@ -178,7 +191,7 @@ Type_Specifier parse_type_specifier(Parser* parser)
         {
             Diagnostic* _diagnostic = diagnostic(
                 DIAGNOSTIC_ERROR, parser->current_token->position, 
-                ":PARSER - SyntaxError: Expected type specifier, got '%s'\n",
+                ":PARSER - SyntaxError: Expected type specifier, got '%s'",
                 parser->current_token->lexeme);
             array_push(parser->diagnostics, _diagnostic); 
             parser->panic = true;
@@ -283,6 +296,8 @@ static AST_Statement* parse_declaration_statement(Parser* parser)
 //
 // EBNF grammar:
 //      if_statement        = 'if' expression 'then' statement ('else' statement)? ;
+//
+// TODO(timo): We could parse all the else-if's into its own array.
 //
 // Arguments
 //      parser: Pointer to initialized Parser.
@@ -389,7 +404,7 @@ AST_Statement* parse_statement(Parser* parser)
 
     Diagnostic* _diagnostic = diagnostic(
         DIAGNOSTIC_ERROR, parser->current_token->position, 
-        ":PARSER - Unreachable: Expected statement starter, got '%s'\n",
+        ":PARSER - Unreachable: Expected statement starter, got '%s'",
         parser->current_token->lexeme);
     array_push(parser->diagnostics, _diagnostic); 
     parser->panic = true;
@@ -398,14 +413,45 @@ AST_Statement* parse_statement(Parser* parser)
 }
 
 
-// Parses a primary expression based on the grammar.
+// Parses a parameter list for a function expression.
 //
-// TODO(timo): What are primary expressions
+// Arguments
+//      parser: Initialized parser.
+// Returns
+//      Pointer to array of parameters.
+static array* parse_parameter_list(Parser* parser)
+{
+    array* parameters = array_init(sizeof (Parameter*));
+    
+    while (parser->current_token->kind != TOKEN_RIGHT_PARENTHESIS &&
+           parser->current_token->kind != TOKEN_EOF)
+    {
+        Token* identifier = parser->current_token;
+        advance(parser);
+
+        expect_token(parser, TOKEN_COLON, ":", false);
+
+        Type_Specifier specifier = parse_type_specifier(parser);
+        array_push(parameters, function_parameter(identifier, specifier));
+
+        // Comma is kept at the end, so we don't have to care about trailing commas.
+        // TODO(timo): But what if there is multiple of them? Then this doesn't
+        // work as expected.
+        if (parser->current_token->kind == TOKEN_COMMA && peek(parser)->kind != TOKEN_RIGHT_PARENTHESIS)
+            advance(parser);
+    }
+
+    return parameters;
+}
+
+
+// Parses a primary expression based on the grammar.
 //
 // EBNF grammar:
 //      primary         = '(' expression ')'
-//                      | '(' parameter_list? ')' '=>' statement ';'
+//                      | function
 //                      | literal ;
+//      function        = '(' parameter_list? ')' '=>' statement ';'
 //      parameter_list  = IDENTIFIER ':' type_specifier ( ',' IDENTIFIER ':' type_specifier )* ;
 //      type_specifier  = 'int' | 'bool' ;
 //      literal         = IDENTIFIER
@@ -439,45 +485,32 @@ static AST_Expression* primary(Parser* parser)
         {
             advance(parser);
 
-            if (parser->current_token->kind == TOKEN_IDENTIFIER && peek(parser)->kind == TOKEN_COLON)
+            // Function expression
+            if ((parser->current_token->kind == TOKEN_IDENTIFIER && peek(parser)->kind == TOKEN_COLON) ||
+                 parser->current_token->kind == TOKEN_RIGHT_PARENTHESIS)
             {
-                array* parameters = array_init(sizeof (Parameter*));
-                
-                do {
-                    if (parser->current_token->kind == TOKEN_COMMA) advance(parser);
-                    
-                    Token* identifier = parser->current_token;
-                    advance(parser);
-                    expect_token(parser, TOKEN_COLON, ":", true);
-                    Type_Specifier specifier = parse_type_specifier(parser);
-                    array_push(parameters, function_parameter(identifier, specifier));
-                } while (parser->current_token->kind == TOKEN_COMMA);
+                array* parameters = parse_parameter_list(parser);
                 
                 expect_token(parser, TOKEN_RIGHT_PARENTHESIS, ")", true);
                 expect_token(parser, TOKEN_ARROW, "=>", false);
+
                 AST_Statement* body = parse_statement(parser);
                 expression = function_expression(parameters, parameters->length, body);
             }
-            else if (parser->current_token->kind == TOKEN_RIGHT_PARENTHESIS)
-            {
-                advance(parser);     
-                expect_token(parser, TOKEN_ARROW, "=>", false);
-                AST_Statement* body = parse_statement(parser);
-                expression = function_expression(NULL, 0, body);
-            }
-            else // Normal parenthesized epxression
+            else // Ordinary parenthesized expression
             {
                 expression = parse_expression(parser);
                 expect_token(parser, TOKEN_RIGHT_PARENTHESIS, ")", true);
             }
-
+            
             break;
         }
         default:
         {
             Diagnostic* _diagnostic = diagnostic(
                 DIAGNOSTIC_ERROR, parser->current_token->position, 
-                ":PARSER - SyntaxError: Invalid token '%s' in primary expression\n",
+                ":PARSER - SyntaxError: Invalid token '%s' in primary expression",
+                // ":PARSER - SyntaxError: Expected expression.",
                 parser->current_token->lexeme);
             array_push(parser->diagnostics, _diagnostic); 
             parser->panic = true;
@@ -743,7 +776,7 @@ static AST_Expression* assignment(Parser* parser)
         {
             Diagnostic* _diagnostic = diagnostic(
                 DIAGNOSTIC_ERROR, expression->position, 
-                ":PARSER - SyntaxError: Invalid assignment target, expected a variable.\n");
+                ":PARSER - SyntaxError: Invalid assignment target, expected a variable.");
             array_push(parser->diagnostics, _diagnostic); 
             // NOTE(timo): In case of invalid assignment target there is really no need
             // for error recovery since we are already at the end of the expression.
